b8e50481d29669099d196a2ca9a078557266abab
NagataKyoshi <Nagatakyoshii@gmail.com>
2021-06-21 13:42:53 +0300

    Question System UI & Code
    Now we can edit question count from inspector and also answer
    if player give answer correct, you can see on debug log and also false answer
    
    each answer, question deleted from list
    
    but we need to do AI Answer system and also player answer camera transition

---
+++ Dumber/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Question> questions;
+    public Question currentQeustion;
+
+    [SerializeField]
+    public Text questionText;
+    public Text answerA;
+    public Text answerB;
+
+    int randomQuestionID; //need for random question but we need to change primary.
+
+    [SerializeField]
+    private float timeBetweenQuestions = 4f;
+
+    private void Start()
+    {
+        SetQuestion();
+    }
+    public void SetQuestion()
+    {
+        randomQuestionID = Random.Range(0, 1); //questions.Count - 1); random yapılmak istenirse
+        currentQeustion = questions[randomQuestionID];
+
+        questionText.text = currentQeustion.question;
+        answerA.text = currentQeustion.answerA;
+        answerB.text = currentQeustion.answerB;
+
+    }
+    //IEnumerator TransitionToNextQuestion()
+    //{
+    //    SetQuestion();
+    //    yield return new WaitForSeconds(timeBetweenQuestions);
+    //    //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
+
+    public void SetAnswer(string correctAnswer)
+    {
+        questions.RemoveAt(randomQuestionID);
+        if (correctAnswer == currentQeustion.trueAnswer)
+        {
+            Debug.Log("Congrats");
+            //add particle system
+            //add audio
+            SetQuestion();
+        }
+        else
+        {
+            Debug.Log("Wrong");
+            //add particle system
+            //add audio
+            SetQuestion();
+        }
+        //StartCoroutine(TransitionToNextQuestion());
+
+    }
+}
---
+++ Dumber/Assets/Scripts/Question.cs
@@ -0,0 +1,18 @@
+﻿
+[System.Serializable]
+public class Question 
+{
+
+    public string question;
+    public string answerA;
+    public string answerB;
+    public string trueAnswer;
+
+    public void Questions(string a, string b, string answer, string questionText)
+    {
+        answerA = a;
+        answerB = b;
+        trueAnswer = answer;
+        question = questionText;
+    }
+}
