318fcbe24a88b626c26da5f6e560774e43a7c3ad
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-06-30 17:11:44 +0300

    Anims added

--- Dumber/Assets/Scripts/Question.cs
+++ Dumber/Assets/Scripts/Question.cs
@@ -6,12 +6,19 @@ public class Question
     public string question;
     public string answerA;
     public string answerB;
+    public string answerC;
+    public string answerD;
+
+    
     public string trueAnswer;
 
-    public void Questions(string a, string b, string answer, string questionText)
+    public void Questions(string a, string b,string c,string d, string answer, string questionText)
     {
         answerA = a;
         answerB = b;
+        answerC = c;
+        answerD = d;
+
         trueAnswer = answer;
         question = questionText;
     }
